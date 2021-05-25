c87579e568f8f389a34fbc84034949f08f3cfcb5
cloesthilaire <cloe.st-hilaire@mail.mcgill.ca>
2021-05-25 11:39:25 -0400

    startup r script

---
+++ R/01_startup.R
@@ -0,0 +1,20 @@
+#### 01 STARTUP ################################################################
+
+# Load packages -----------------------------------------------------------
+
+library(tidyverse)
+library(lubridate)
+library(upgo)
+library(strr)
+library(sf)
+library(stringr)
+library(tidyr)
+
+
+# Set global variables ----------------------------------------------------
+
+#if (Sys.info()["sysname"] != "Windows") {plan(multiprocess)}
+col_palette <- 
+  c("#FF6600", "#CC6699", "#3399CC", "#FFCC66", "#074387", "#6EEB83", "#008A43", "#FFD500", "#A80858")
+
+scales::show_col(col_palette)
