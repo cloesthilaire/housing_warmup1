efb90483557a058fe6f103c92c9081b2bddc05e6
Alexia-WR <alexia.wildhaber-riley@email.com>
2021-07-02 15:36:08 -0400

    Friday push (accidentely pushed before commiting, sorry!!)

--- R/11_commercial_gentrification.R
+++ R/11_commercial_gentrification.R
@@ -9,22 +9,27 @@ load("output/province.Rdata")
 # Load the Ãtablissements alimentaires data from DonnÃ©es Montreal
 # dataset is from 1988 to 2021.06.28
 
-food_businesses_sf <-
+food_businesses_raw <-
   read_sf("data/businesses/businesses.shp") %>% 
   st_transform(32618) %>% 
-  mutate(DATE = ymd(date_statu)) %>% 
-  mutate_at(vars(DATE), funs(year, month, day))
+  mutate(date = ymd(date_statu))
 
-food_businesses <-
+food_businesses_sf <- 
+  food_businesses_raw %>% 
+  select(-state, -date_statu, -latitude, -longitude, -x, -y) %>% 
+  relocate(geometry, .after = last_col()) %>% 
+  set_names(c("id", "name", "address", "city", "type", "statut", "date", "geometry"))
+
+food_businesses <- 
   food_businesses_sf %>% 
   st_drop_geometry()
 
-#Save output -------------------------------------------------------------
+# save output
+save(food_businesses, food_businesses_sf, file = "output/food_businesses.Rdata")
 
-save(food_business, file = "output/food_business")
 
 # This dataset has classified businesses into the following categories: 
-# Ãcole/mesure alimentaires, Ãpicerie, Ã©picerie avec prÃ©paration, Ã©vÃ¨nement spÃ©ciaux, 
+# Ecole/mesure alimentaires, Ãpicerie, Ã©picerie avec prÃ©paration, Ã©vÃ¨nement spÃ©ciaux, 
 # aliments naturels, Atelier de conditionnement de produits de la pÃªche, autres, 
 # bar laitier, bar laitiers saisonnier, bar salon, taverne, boucherie, boucherie-Ã©picerie, 
 # boulangerie, brasserie, cabane Ã  sucre, (cafÃ©, thÃ©, infusion, tisane = toute une catÃ©gorie), 
@@ -53,16 +58,76 @@ pal <- wes_palette("Zissou1", 10, type = c("continuous"))
 
 # Determining how many businesses are inventoried per year
 
-food_businesses_sf %>% 
-  filter(year == 2005) %>% 
-  count(statut)
+food_businesses%>% 
+  mutate(date=year(date)) %>% 
+  count(date) %>% 
+  View()
   
 
-# repeat this process for every year to find: 1988=10, 1989=28, 1990=35, 1991=16
+# 1988=10, 1989=28, 1990=35, 1991=16
 # 1992=12, 1993=1460, 1994=93, 1995=105, 1996=84, 1997=72, 1998 = 93, 1999= 86,
 # 2000= 121, 2001=171, 2002=234, 2003=182, 2004=201, 2005=147, 2006=353, 2007=817,
 # 2008=1841, 2009=2162, 2010=2547, 2011=2017, 2012=2731, 2013=2705, 2014=2835,
 # 2015=3030, 2016=2931, 2017=2882, 2018=3628, 2019=4161, 2020=3334, 2021=1807
+
+# To clean the groups before grouping
+
+types_to_remove <- c("Atelier de conditionnement de produits de la pêche", "Cabane à sucre",
+                     "Camion de distribution de produits carnés", "Camion de distribution de produits de la pêche",
+                     "Camion de distribution de produits laitiers", "Camp de vacances", "Hôpital", "Site d'eau vendue au volume",
+                     "Véhicule livraison", "Vendeur itinérant", "Distributrice automatique")
+
+food_businesses_test <- 
+  food_businesses %>% 
+  filter(!type %in% types_to_remove) %>% 
+  filter(!str_detect(type, "Distributeur en gros")) %>% 
+  filter(!str_detect(type, "Entrepôt")) %>% 
+  filter(!str_detect(type, "Usine"))
+
+#remove more categories?
+
+groups <- 
+  food_businesses_test %>% 
+  select(-city, -statut) %>% 
+  arrange(date) %>% 
+  group_by(address) %>% 
+  mutate(group_id = row_number()) 
+
+# Load grouped data
+load("output/grouped_addresses.Rdata")
+
+
+# Statistical Analysis ---------------------------------------------------------
+
+# Bar charts
+
+# This code does not work, still keeping it for further reference
+#food_businesses %>% 
+#  mutate(date = year(date)) %>% 
+#  ggplot(aes())+
+#geom_bar(mapping=NULL,x="statut", stat="identity", position ="stack",
+#         width = NULL,
+#         na.rm = FALSE,
+#         orientation = NA,
+#         show.legend = NA)+
+#  facet_wrap(year)
+
+# This graph works
+
+food_businesses %>% 
+  mutate(date = year(date)) %>% 
+  ggplot()+
+  geom_histogram(aes(x = date, fill = statut), stat="count")
+
+# Proportional status per year bar graph
+food_businesses %>% 
+  mutate(date = year(date)) %>%
+  filter(date >= 2010) %>% 
+  count(date, statut) %>% 
+  ggplot(aes(x = date, y = n, fill = statut)) + 
+  geom_bar(position = "fill",stat = "identity")
+
+
 
 # Map 1: 1988-1993
 # 1561 datapoints
